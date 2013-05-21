6bf07c26a6d47137bd0770a4f1c6a5e83ede6ea9
PlasmaSheep <plasmasheep@gmail.com>
2013-05-20 17:49:12 -0700

    Rewriting baogame with better logic

--- BaoGame.java
+++ BaoGame.java
@@ -1,3 +1,4 @@
+
 import java.util.Arrays;
 
 public class BaoGame {
@@ -7,7 +8,7 @@ public class BaoGame {
    
     public BaoGame() {
         board = new Pit[4][8];
-        players = new int[2]; //Player 1 on top, player 2 not
+        players = new int[2]; //Player 0 on top, player 1 not
         Arrays.fill(players, 22);
         turn = 1;
         
@@ -19,8 +20,54 @@ public class BaoGame {
         board[1][5].setSeeds(2);
         board[1][6].setSeeds(2);
     }
-   
-    private boolean placeSeed(int row, int col, int player) {
+    
+    private int getTopRow(int player) {
+        if(player == 0) {
+            return 0;
+        } if(player == 1) {
+            return 2;
+        }
+    }
+    
+    private boolean playerHasNonSingletons(int player) {
+        for(int r = getTopRow(player); r <= getTopRow(player) + 1; r+) {
+            for(int c = 0; c < 9; c++) {
+                if(board[r][c].getSeeds() > 1) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+            
+    }
+    public void Play() {
+        int winner = 0;
+        int player = 0;
+        while(winner == 0) {
+            if(isRowEmpty(player) || !playerHasNonSingletons(player)) {
+                //player's inner row is empty
+                winner = player % 2;
+                break;
+            }
+            if(players[player] > 0) { //Namua
+                //TODO: User input: where to put the seed
+                //restrictions: must capture if can capture, must have/
+                //seeds already there
+                Loc loc = getSowLoc(player);
+                Loc aloc = loc.getLocAcross();
+                players[player]--;
+                if(board[aloc.getRow()][aloc.getCol()].getSeeds() > 1) {
+                    //Capture happened
+                    Loc sowStart = loc.getNearestKichwa();
+                    if(!loc.isKichwa() && !loc.isKimbi() {
+                        Loc sowStart = getKichwaChoice(player);
+                    }
+                    
+                
+                
+                
+    /*private boolean placeSeed(int row, int col, int player) {
         if(players[player] > 0) {
             board[row][col].addSeeds(1);
             players[player]--;
@@ -86,6 +133,5 @@ public class BaoGame {
     
     private Pit getPit(Loc loc) {
         return board[loc.getRow()][loc.getCol()];
-    }
+    }*/
 }
-
